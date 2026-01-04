7c8e58609152b7fb02183e8e44798223352b8686
Lyu6asha <lyu6asha@gmail.com>
2026-01-04 21:43:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,5 @@
 
 let background_img = document.querySelector('.header');
 
-background_img.style.backgroundImage = 'url(https://downloader.disk.yandex.ru/preview/46b2dafb29437a93287cb874b2e4f2199f479656dffd21d944309d2f8ab44963/695ac303/Xod_Qu8gpQf_b_45UYJKMJ5O7jYxidwxm9BUMdBzumHTkXrK6FsLe4Y6vS8jLjh6IAPI8TefQ3VlIaY_KUaP0Q%3D%3D?uid=0&filename=%D0%91%D0%B5%D1%80%D0%B5%D0%B3%20%D0%BC%D0%BE%D1%80%D1%8F%20-%20640x428.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v3&size=2048x2048)';
+background_img.style.backgroundImage = 'url(https://www.perfectstock.ru/files/preview/1280x854/1b1793fb8d95a9b145f379cbe45a9949-l-21583943527x5jlislokb0xxbibmndwaajah41xovcraylqbjclixc1vd9t0bmfmbm96cat9h2cfchrjmh2arbc84xcpkt95we3igaxzmgkpxdz.jpg?size=medium)';
+
