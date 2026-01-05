34e499c5800b42ae4c48ab0de30512c9b0df242f
Lyu6asha <lyu6asha@gmail.com>
2026-01-05 11:55:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,11 @@
 
 //smoothly(background_img, 'textContent', 'Херота');
 
-let background_img = document.querySelector('.header');
+let background_img = document.querySelector('.body');
 
 background_img.style.backgroundImage = 'url(https://www.perfectstock.ru/files/preview/1280x854/1b1793fb8d95a9b145f379cbe45a9949-l-21583943527x5jlislokb0xxbibmndwaajah41xovcraylqbjclixc1vd9t0bmfmbm96cat9h2cfchrjmh2arbc84xcpkt95we3igaxzmgkpxdz.jpg?size=medium)';
 
 let phrase = document.querySelector('.button');
-phrase.textContent = 'Херня';
+phrase.textContent = 'Хер..';
+
 
